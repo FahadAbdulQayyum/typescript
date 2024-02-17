ed9dcb1cac7df9c581d82d846023e545b85ff564
Fahad <fahadabdulqayyum@focuspass.com>
2024-02-18 00:39:19 +0500

    completed till 11

--- a.ts
+++ a.ts
@@ -21,5 +21,52 @@ console.log("Albert Einstein once said, “A person who never made a mistake nev
 /*********************************************************************************/
 // 5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
 
-const famousQuote : string = "Albert Einstein once said, “A person who never made a mistake never tried anything new."
+const famousQuote : string = "Albert Einstein once said, “A person who never made a mistake never tried anything new.”"
 console.log(famousQuote)
+
+/*********************************************************************************/
+// 6. Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+
+const strippedName : string = "   Fahad    "
+console.log(strippedName)
+console.log(strippedName.trim())
+
+/*********************************************************************************/
+// 7. Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+const a : number = 5;
+const b : number = 3;
+const c : number = 4;
+const d : number = 2;
+
+const addition : number = a + b;
+const multiplication : number = c * d;
+const division : number = (addition * d) / d;
+
+console.log('Addition:', addition);
+console.log('Multiplication:', multiplication);
+console.log('Division:', division);
+
+/*********************************************************************************/
+// 8. You should create four lines that look like this:
+
+console.log('********************************************************************************')
+console.log("console.log(5 + 3)")
+console.log('********************************************************************************')
+
+/*********************************************************************************/
+// 9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+const favoriteNumber : number = 40;
+console.log("I like this number: "+favoriteNumber)
+
+/*********************************************************************************/
+// 10. Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+// This is how we write comments in programming languages for reminding ourselves after years
+// This is generally written for the very first time when one learns coding.
+console.log("Hello World!");
+
+/*********************************************************************************/
+// 11. Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
