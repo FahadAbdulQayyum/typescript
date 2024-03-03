2ea692a01e53d4410b1e246c2a32ddafa8df29ef
Fahad <fahadabdulqayyum@focuspass.com>
2024-03-03 15:33:58 +0500

    Now !Q31 completed

--- Q31.js
+++ Q31.js
@@ -1,10 +1,10 @@
 // 31. No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
-var users = ["Fahad", "Aftab", "Haider"];
+// const users : Array<String> = ["Fahad", "Aftab", "Haider"]; // Non-empty array
+var users = []; // Empty array
 if (users.length === 0) {
     console.log("We need to find some users!");
 }
 else {
-    // users.forEach(user => console.log("Welcome "+user+" !"));
     var str_1 = "";
     str_1 += "Welcome ";
     users.forEach(function (user) {
@@ -15,12 +15,7 @@ else {
     var filteredStr = str_1.slice(0, str_1.length - 2);
     var splitted = filteredStr.split(" ");
     splitted.splice(splitted.length - 1, splitted.length, "and");
-    // console.log('splitted', splitted.push(str.split[str.length-1]));
     var splitt = str_1.split(", ")[splitted.length - 2];
-    // console.log('---',splitt)
-    console.log('---', typeof splitt);
     splitted.push(splitt + "!");
-    console.log('splitted', splitted.join(" "));
-    // console.log('splitted', splitted);
-    console.log(str_1);
+    console.log(splitted.join(" "));
 }
--- Q31.ts
+++ Q31.ts
@@ -1,10 +1,10 @@
 // 31. No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
 
-const users : Array<String> = ["Fahad", "Aftab", "Haider"];
+const users : Array<String> = ["Fahad", "Aftab", "Haider"]; // Non-empty array
+// const users : Array<String> = []; // Empty array
 if(users.length === 0){
     console.log("We need to find some users!");
 }else{
-    // users.forEach(user => console.log("Welcome "+user+" !"));
     let str: string = "";
     str += "Welcome ";
     users.forEach(user => {
@@ -15,12 +15,7 @@ if(users.length === 0){
     let filteredStr = str.slice(0, str.length-2);
     let splitted = filteredStr.split(" ")
     splitted.splice(splitted.length-1, splitted.length, "and",)
-    // console.log('splitted', splitted.push(str.split[str.length-1]));
     let splitt = str.split(", ")[splitted.length-2]
-    // console.log('---',splitt)
-    console.log('---',typeof splitt)
     splitted.push(splitt+"!")
-    console.log('splitted', splitted.join(" "));
-    // console.log('splitted', splitted);
-console.log(str)
+    console.log(splitted.join(" "));
 }
