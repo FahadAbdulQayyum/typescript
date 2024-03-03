f3ec2742ec1ecd59b10000a7e1b3f6d4264a21b5
Fahad <fahadabdulqayyum@focuspass.com>
2024-03-03 17:03:17 +0500

    removed extra comments for Q32

---
+++ Q33.ts
@@ -0,0 +1,6 @@
+// 33. Ordinal Numbers: Ordinal numbers indicate their position in a array, such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
+const ordinalNumbers: Array<number> = [2, 5, 7, 10, 33, 59, 89, 91, 99, 103, 110];
+
+for (let i = 0; i < ordinalNumbers.length; i++) {
+    console.log(ordinalNumbers[i]);
+}
