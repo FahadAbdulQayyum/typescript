96bfda3b6587fedcb764fcdba43fc36b28813784
Fahad <fahadabdulqayyum@focuspass.com>
2024-03-03 17:01:29 +0500

    removed extra comments for Q32

--- Q32.ts
+++ Q32.ts
@@ -13,41 +13,6 @@ for (let i = 0; i < currentUsers.length; i++) {
     }
 }
 
-let lower = notAcceptedUsers.map(user => user.toLowerCase());
-newUsers.map(user => !lower.includes(user.toLowerCase())?console.log(user+" is available."):'');
+let lowerNotAcceptedUsers = notAcceptedUsers.map(user => user.toLowerCase());
+newUsers.map(user => !lowerNotAcceptedUsers.includes(user.toLowerCase())?console.log(user+" is available."):'');
 notAcceptedUsers.forEach(user => console.log(user.charAt(0).toUpperCase()+user.slice(1,).toLowerCase()+' will need to enter new username.'));
-
-// for (let i = 0; i < currentUsers.length; i++) {
-//     for (let j = 0; j < newUsers.length; j++) {
-//         if (currentUsers[i].toLowerCase() === newUsers[j].toLowerCase()) {
-//             console.log(currentUsers[i].toLowerCase() + " is already taken!");
-//         }
-//     }
-//     console.log(newUsers[i].toLowerCase() + " is available!");
-// }
-
-
-// currentUsers.forEach(currentUser => newUsers.forEach(newUser => {
-//     if(currentUser.toLowerCase() === newUser.toLowerCase()){
-//         console.log(currentUser+" will need to enter a new username.");
-//     }else{
-//         console.log(currentUser+" is available!");
-//     }
-// }))
-
-// for (let i = 0; i < currentUsers.length; i++) {
-//     console.log(currentUsers[i], ' ---- '+ newUsers[i]);
-//     if(currentUsers[i].toLowerCase() === newUsers[i].toLowerCase()) {
-//         console.log(newUsers[i]+" will need to enter a new username.");
-//     }else {
-//         console.log(newUsers[i]+" is available!");
-//     }
-// }
-
-// for (let i = 0; i < currentUsers.length; i++) {
-//     if(currentUsers.includes(newUsers[i].toString().toLowerCase())){
-//         console.log(currentUsers[i]+" will need to enter a new username.");
-//     }else{
-//         console.log(currentUsers[i]+" is available.");
-//     }
-// }
